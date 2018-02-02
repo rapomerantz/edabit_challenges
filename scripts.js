17eaa1831ba9968cdbc85a73f3aee8b2ab85df2a
Atticus Pomerantz <rapomerantz@gmail.com>
2018-02-01 19:36:20 -0600

    count wordsin string

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
 console.log( 'JS LINKED' );
+
+
+
+//Count the number of words in a sentence
+let quote = "Four score and seven years ago"
+
+function countWords(quote){
+	return quote.split (' ').length
+
+}
+
+console.log(countWords(quote));
+//Count the number of words in a sentence
